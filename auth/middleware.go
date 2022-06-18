81eb78da7027c74a54ffd36d7cf09c06600eb651
Warren Snipes <contact@warrensnipes.dev>
2022-06-18 03:14:55 -0400

    added auth context helper functions

---
+++ auth/middleware.go
@@ -0,0 +1,28 @@
+package auth
+
+import (
+	"context"
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
+
+func AuthContextMiddleware(auth *Auth) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx := context.WithValue(c.Request.Context(), "Auth", auth)
+		c.Request = c.Request.WithContext(ctx)
+		c.Next()
+	}
+}
+
+func AuthFromContext(ctx context.Context) (*Auth, error) {
+	auth := ctx.Value("Auth")
+	if auth == nil {
+		err := fmt.Errorf("could not retrieve auth.Auth")
+		return nil, err
+	}
+
+	if gc, ok := auth.(*Auth); ok {
+		return gc, nil
+	}
+	return nil, errors.New("auth.Auth has wrong type")
+}
