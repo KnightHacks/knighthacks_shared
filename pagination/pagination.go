efad28c7abed375a07d278d9cd3e186c1bd8d0bf
Warren Snipes <contact@warrensnipes.dev>
2022-06-20 00:50:58 -0400

    implemented functions to manage cursors

--- pagination/pagination.go
+++ pagination/pagination.go
@@ -2,11 +2,43 @@ package pagination
 
 import (
 	"context"
+	"encoding/base64"
 	"fmt"
 	"github.com/99designs/gqlgen/graphql"
+	"github.com/KnightHacks/knighthacks_shared/models"
 )
 
-func Pagination(ctx context.Context, obj interface{}, next graphql.Resolver, maxLength int) (res interface{}, err error) {
+// TODO: should cursors be encrypted? is it worth it?
+
+var ZeroString = "0"
+
+func DecodeCursor(cursor *string) error {
+	if cursor == nil {
+		cursor = &ZeroString
+	} else {
+		bytes, err := base64.StdEncoding.DecodeString(*cursor)
+		if err != nil {
+			return err
+		}
+		bytesString := string(bytes)
+		cursor = &bytesString
+	}
+	return nil
+}
+
+func GetPageInfo(first string, last string) *models.PageInfo {
+	format := func(s string) string {
+		bytes := []byte(s)
+		return base64.StdEncoding.EncodeToString(bytes)
+	}
+
+	return &models.PageInfo{
+		StartCursor: format(first),
+		EndCursor:   format(last),
+	}
+}
+
+func Pagination(ctx context.Context, _ interface{}, next graphql.Resolver, maxLength int) (res interface{}, err error) {
 	fieldContext := graphql.GetFieldContext(ctx)
 	first := fieldContext.Args["first"].(int)
 	if first > maxLength {
