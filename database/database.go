a38ef9be669d99e6a9a6667c612753bcfdb5194c
Warren Snipes <contact@warrensnipes.dev>
2022-06-26 14:09:54 -0400

    implemented Queryable

---
+++ database/database.go
@@ -0,0 +1,15 @@
+package database
+
+import (
+	"context"
+	"github.com/jackc/pgconn"
+	"github.com/jackc/pgx/v4"
+)
+
+//Queryable this is a sort of abstraction between pgx.Pool and Transactions, so we are able to pass either one
+type Queryable interface {
+	Exec(ctx context.Context, sql string, arguments ...interface{}) (commandTag pgconn.CommandTag, err error)
+	Query(ctx context.Context, sql string, args ...interface{}) (pgx.Rows, error)
+	QueryRow(ctx context.Context, sql string, args ...interface{}) pgx.Row
+	QueryFunc(ctx context.Context, sql string, args []interface{}, scans []interface{}, f func(pgx.QueryFuncRow) error) (pgconn.CommandTag, error)
+}
