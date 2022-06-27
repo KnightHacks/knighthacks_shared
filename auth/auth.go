86d3778bc16d11d3da4aabf45ce02df31d0bc9aa
Warren Snipes <contact@warrensnipes.dev>
2022-06-26 21:25:05 -0400

    implemented constructors to remove duplicate code for initializing Auth using env vars

--- auth/auth.go
+++ auth/auth.go
@@ -8,9 +8,11 @@ import (
 	"errors"
 	"fmt"
 	"github.com/KnightHacks/knighthacks_shared/models"
+	"github.com/KnightHacks/knighthacks_shared/utils"
 	"github.com/golang-jwt/jwt"
 	"github.com/google/go-github/v45/github"
 	"golang.org/x/oauth2"
+	githubOAuth "golang.org/x/oauth2/github"
 	"io"
 	"strconv"
 	"time"
@@ -40,6 +42,10 @@ type UserClaims struct {
 	jwt.StandardClaims
 }
 
+func NewAuthWithEnvironment() (*Auth, error) {
+	return NewAuth(utils.GetEnvOrDie("JWT_SIGNING_KEY"), utils.GetEnvOrDie("AES_CIPHER"), NewOAuthMap())
+}
+
 func NewAuth(signingKey string, cipher32Bit string, configMap map[models.Provider]oauth2.Config) (*Auth, error) {
 	newCipher, err := aes.NewCipher([]byte(cipher32Bit))
 	if err != nil {
@@ -50,6 +56,29 @@ func NewAuth(signingKey string, cipher32Bit string, configMap map[models.Provide
 		return nil, err
 	}
 	return &Auth{ConfigMap: configMap, signingKey: []byte(signingKey), gcm: gcm}, nil
+}
+
+func NewOAuthMap() map[models.Provider]oauth2.Config {
+	return map[models.Provider]oauth2.Config{
+		//TODO: implement gmail auth, github is priority
+		//auth.GmailAuthProvider: {
+		//	ClientID:     "",
+		//	ClientSecret: "",
+		//	Endpoint:     oauth2.Endpoint{},
+		//	RedirectURL:  "",
+		//	Scopes:       nil,
+		//},
+		models.ProviderGithub: {
+			ClientID:     utils.GetEnvOrDie("OAUTH_GITHUB_CLIENT_ID"),
+			ClientSecret: utils.GetEnvOrDie("OAUTH_GITHUB_CLIENT_SECRET"),
+			RedirectURL:  utils.GetEnvOrDie("OAUTH_GITHUB_REDIRECT_URL"),
+			Endpoint:     githubOAuth.Endpoint,
+			Scopes: []string{
+				"read:user",
+				"user:email",
+			},
+		},
+	}
 }
 
 func (a *Auth) GetAuthCodeURL(provider models.Provider, state string) string {
